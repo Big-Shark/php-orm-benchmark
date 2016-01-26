6e089d4c8c9064ac8dcb05bf4624d5d393976d58
githubjeka <et.coder@gmail.com>
2016-01-26 09:20:30 +0300

    Init yii change to production state.

--- yii2/TestDefault.php
+++ yii2/TestDefault.php
@@ -13,8 +13,8 @@ class TestDefault extends AbstractTest
 
     public function initialization()
     {
-        defined('YII_DEBUG') or define('YII_DEBUG', true);
-        defined('YII_ENV') or define('YII_ENV', 'dev');
+        defined('YII_DEBUG') or define('YII_DEBUG', false);
+        defined('YII_ENV') or define('YII_ENV', 'prod');
 
         require(__DIR__ . '/vendor/autoload.php');
         require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
